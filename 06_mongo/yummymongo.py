89d224eda35791fe28750af41da896443f334f72
Derek Chan <ychan3@stuy.edu>
2019-03-01 00:25:41 -0500

    added 06

---
+++ 06_mongo/yummymongo.py
@@ -0,0 +1,36 @@
+# Yin On Chan
+# SoftDev pd8
+# K06 -- Yummy Mongo Py
+# 02/28/2019
+
+import pymongo, pprint
+
+SERVER_ADDR = "68.183.98.209"
+connection = pymongo.MongoClient(SERVER_ADDR)
+db = connection.test
+collection = db.restaurants
+
+def by_borough(borough):
+    for doc in collection.find( {"borough": borough} ):
+        pprint.pprint(doc)
+        print('\n')
+
+def by_zip(zipcode):
+    for doc in collection.find( {"address.zipcode": zipcode} ):
+        pprint.pprint(doc)
+        print('\n')
+
+def by_zip_and_grade(zipcode, grade):
+    for doc in collection.find( { '$and': [ {"address.zipcode": zipcode}, {"grades.grade": grade} ] } ):
+        pprint.pprint(doc)
+        print('\n')
+
+def by_score_threshold(score):
+    for doc in collection.find( {"grades.score": {'$lt': score} } ):
+        pprint.pprint(doc)
+        print('\n')
+
+def by_cuisine_and_street(cuisine, street): 
+    for doc in collection.find( { '$and': [ {"cuisine": cuisine}, {"address.street": street} ] } ):
+        pprint.pprint(doc)
+        print('\n')
